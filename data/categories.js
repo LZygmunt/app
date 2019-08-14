985d3d10c0c0614b9290abe383a40bcccc64f9e8
Łukasz <34583194+LZygmunt@users.noreply.github.com>
2019-08-14 12:29:20 +0200

    app

---
+++ data/categories.js
@@ -0,0 +1,121 @@
+export const categories = [
+  {
+    id: 1,
+    name: "Alcohol",
+    category: "alcohol",
+    subCategories: ["alcohol", "beer", "wine"],
+    parentId: 17
+  },
+  {
+    id: 2,
+    name: "Breadstuff",
+    category: "breadstuff",
+    subCategories: ["breadstuff"],
+    parentId: 0
+  },
+  {
+    id: 3,
+    name: "Meat",
+    category: "meat",
+    subCategories: ["meat"],
+    parentId: 0
+  },
+  {
+    id: 4,
+    name: "Noodles",
+    category: "noodles",
+    subCategories: ["noodles"],
+    parentId: 0
+  },
+  {
+    id: 5,
+    name: "Desserts",
+    category: "desserts",
+    subCategories: ["desserts"],
+    parentId: 0
+  },
+  {
+    id: 6,
+    name: "Fruits",
+    category: "fruits",
+    subCategories: ["fruits"],
+    parentId: 0
+  },
+  {
+    id: 7,
+    name: "Juice",
+    category: "juice",
+    subCategories: ["juice"],
+    parentId: 17
+  },
+  {
+    id: 8,
+    name: "Beer",
+    category: "beer",
+    subCategories: ["beer"],
+    parentId: 1
+  },
+  {
+    id: 9,
+    name: "Wine",
+    category: "wine",
+    subCategories: ["wine"],
+    parentId: 1
+  },
+  {
+    id: 10,
+    name: "Flakes",
+    category: "flakes",
+    subCategories: ["flakes"],
+    parentId: 0
+  },
+  {
+    id: 11,
+    name: "Seafood",
+    category: "seafood",
+    subCategories: ["seafood", "fish"],
+    parentId: 0
+  },
+  {
+    id: 12,
+    name: "Fish",
+    category: "fish",
+    subCategories: ["fish"],
+    parentId: 11
+  },
+  {
+    id: 13,
+    name: "Canned Food",
+    category: "canned_food",
+    subCategories: ["canned_food"],
+    parentId: 0
+  },
+  {
+    id: 14,
+    name: "Honeys",
+    category: "honeys",
+    subCategories: ["honeys"],
+    parentId: 0
+  },
+  {
+    id: 15,
+    name: "Cheese",
+    category: "cheese",
+    subCategories: ["cheese"],
+    parentId: 0
+  },
+  {
+    id: 16,
+    name: "Cake",
+    category: "cake",
+    subCategories: ["cake"],
+    parentId: 0
+  },
+  {
+    id: 17,
+    name: "Drinks",
+    category: "drinks",
+    subCategories: ["drinks", "alcohol", "beer", "wine", "juice"],
+    parentId: 0
+  }
+];
